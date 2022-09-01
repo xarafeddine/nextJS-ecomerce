e581a0c4d385f070570c835388066135179fbe31
xarafeddine <nu01280@gmail.com>
2022-09-01 19:00:19 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Xaraf's Shop</title>
+        <title>Ordinary Shop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcom to Xaraf's Shop</h1>
+        <h1 className={styles.title}>Welcom to Ordinary Shop</h1>
 
         <p className={styles.description}>
-          the best fake online shop on the web!
+          the most expensive Ordinary shop on the web!
         </p>
 
         <ul className={styles.grid}>
@@ -26,7 +26,7 @@ export default function Home() {
             return (
               <li key={id} className={styles.card}>
                 <a href="">
-                  <img src={image} alt={title} />
+                  <image src={image} alt={title} />
                   <h2>{title}</h2>
                   <p>$ {price}</p>
                   <p>{description}</p>
